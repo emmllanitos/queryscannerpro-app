e8d4b8c1dea9a0bdf058e751b6aa4f9d80972553
Edwar Mauricio Mayorga Llanos <emmllanitos@hotmail.com>
2023-10-10 17:42:25 -0500

    change model result to json

--- queryscannerpro/models.py
+++ queryscannerpro/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class QueryFile(models.Model):
-    filename = models.CharField(max_length=200)
-    result = models.TextField(blank=True)
-    user = models.CharField(blank=True, max_length=200)
+    filename = models.CharField(max_length=100)
+    result = models.JSONField()
+    user = models.CharField(blank=True, max_length=100)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
